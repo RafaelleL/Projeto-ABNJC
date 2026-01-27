84922c7e7ed263edc641c8f8ea8869de2d0dad24
Rafaelle lorena <rafaellelorenacosta@gmail.com>
2026-01-27 20:16:25 -0300

    Adicionando Arquivos

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function initHeaderScripts() {
+  // Abre/fecha menu mobile e troca ícone
+  const menuIcons = document.querySelectorAll('.mobile-menu-icon button'); 
+  const menuMobile = document.querySelector('.mobile-menu');
+  const icon = document.querySelector('.icon');
+
+  menuIcons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      menuMobile.classList.toggle('open');
+
+      if (menuMobile.classList.contains('open')) {
+        icon.src = "../assets/img/Páginas/Header/close-black.svg";
+      } else {
+        icon.src = "../assets/img/Páginas/Header/menu_black.svg";
+      }
+    });
+  });
+
+  // Dropdown no mobile
+  const mobileDropdowns = document.querySelectorAll('.mobile-menu .nav-item.dropdown > a');
+
+  mobileDropdowns.forEach(drop => {
+    drop.addEventListener('click', function(e) {
+      e.preventDefault();
+      const menu = this.nextElementSibling; 
+      menu.classList.toggle('open');
+    });
+  });
+}
