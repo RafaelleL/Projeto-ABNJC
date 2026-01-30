6946d91d0572908d231d94a68fa0761846968155
Rafaelle lorena <rafaellelorenacosta@gmail.com>
2026-01-29 21:23:56 -0300

    Alterando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,27 @@
-function initHeaderScripts() {
-  // Abre/fecha menu mobile e troca ícone
-  const menuIcons = document.querySelectorAll('.mobile-menu-icon button'); 
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "assets/img/Páginas/Header/menu_black.svg";
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = "assets/img/Páginas/Header/close-black.svg";
+  }
+}
+
+// Dropdown no mobile 
+const mobileDropdowns = document.querySelectorAll('.mobile-menu .nav-item.dropdown > a'); mobileDropdowns.forEach(drop => { drop.addEventListener('click', function (e) { e.preventDefault(); 
+  const menu = this.nextElementSibling; 
+  menu.classList.toggle('open'); 
+}); 
+}); 
+
+window.addEventListener('resize', () => {
   const menuMobile = document.querySelector('.mobile-menu');
   const icon = document.querySelector('.icon');
 
-  menuIcons.forEach(btn => {
-    btn.addEventListener('click', () => {
-      menuMobile.classList.toggle('open');
-
-      if (menuMobile.classList.contains('open')) {
-        icon.src = "assets/img/Páginas/Header/close-black.svg";
-      } else {
-        icon.src = "assets/img/Páginas/Header/menu_black.svg";
-      }
-    });
-  });
-
-  // Dropdown no mobile
-  const mobileDropdowns = document.querySelectorAll('.mobile-menu .nav-item.dropdown > a');
-
-  mobileDropdowns.forEach(drop => {
-    drop.addEventListener('click', function(e) {
-      e.preventDefault();
-      const menu = this.nextElementSibling; 
-      menu.classList.toggle('open');
-    });
-  });
-}
+  if (window.innerWidth > 730) {
+    menuMobile.classList.remove('open');
+    icon.src = "assets/img/Páginas/Header/menu_black.svg";
+  }
+});
